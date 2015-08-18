78012f792b0c0a109c04eae1a12de7cce5613f15
Nikolay Georgiev <n.georgiev@testbirds.de>
2015-08-18 12:52:18 +0300

    fixed filename filter

--- Utils/SlugFilter.php
+++ Utils/SlugFilter.php
@@ -4,26 +4,20 @@ namespace Thrace\MediaBundle\Utils;
 
 class SlugFilter
 {
-    public static function filter($value, $separator = '-')
+    public static function filter ($filename = '')
     {
-        if (static::isUnicodeEnabled()){
-            $pattern = '/[^\p{L}\p{N}\s]/u';
-        } else {
-            $pattern = '/[^a-zA-Z0-9/s]/u';
-        }
+        $filename = strip_tags($filename);
+        $filename = preg_replace('/[\r\n\t ]+/', ' ', $filename);
+        $filename = preg_replace('/[\"\*\/\:\<\>\?\'\|]+/', ' ', $filename);
+        $filename = strtolower($filename);
+        $filename = html_entity_decode( $filename, ENT_QUOTES, "utf-8" );
+        $filename = htmlentities($filename, ENT_QUOTES, "utf-8");
+        $filename = preg_replace("/(&)([a-z])([a-z]+;)/i", '$2', $filename);
+        $filename = str_replace(' ', '-', $filename);
+        $filename = rawurlencode($filename);
+        $filename = str_replace('%', '-', $filename);
 
-        // allowing only alnum
-        $value = preg_replace($pattern, ' ', (string) $value);
-
-        $chars = '\\s`~!@#$%\^&*()\\-=_+\\[\\]{};\':",.\\/\<\>?|\\\\„”';
-        $value = mb_strtolower($value, 'UTF-8');
-        $value = preg_replace("/[{$chars}]+/u", $separator, $value);
-        $value = preg_replace("/[-]$/u", '', $value);
-        return $value;
+        return $filename;
     }
 
-    protected static function isUnicodeEnabled()
-    {
-        return (@preg_match('/\pL/u', 'a')) ? true : false;
-    }
 }
